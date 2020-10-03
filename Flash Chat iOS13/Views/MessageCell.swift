371c0afc53388dcf5d9ce02ef79aa837fdba6d4a
Utsav Tulsyan <utsav.tulsyan@publicissapient.com>
2020-10-03 12:27:31 +0530

    added login and chat functionality

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,33 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Utsav Tulsyan on 02/10/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var meAvatar: UIImageView!
+    @IBOutlet weak var youAvatar: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
